9f5804b3a5121d752c59d8ae12c6d63ab6f9495d
Louis <louis.leslie@sicepat.com>
2024-09-11 10:47:14 +0700

    Add data source for typeorm

---
+++ db/data-source.ts
@@ -0,0 +1,12 @@
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  url: process.env.DATABASE_URI,
+  type: 'postgres',
+  entities: ['../entity/**/*.ts'],
+  migrations: ['../migration/**/*.ts'],
+  subscribers: ['..//subscriber/**/*.ts'], // Path to subscriber files
+});
--- index.ts
+++ index.ts
@@ -1,18 +1,33 @@
-import express from "express";
 import cors from 'cors';
+import * as dotenv from 'dotenv';
+import express from 'express';
 import morgan from 'morgan';
 
-const app = express();
-const port = 5000;
+import { AppDataSource } from './db/data-source';
 
-app.use(express.json());
-app.use(cors());
-app.use(morgan('tiny'))
+const bootstrap = async () => {
+  dotenv.config();
+  await AppDataSource.initialize().catch((err) => {
+    console.error('err', err);
+    throw err;
+  });
+  console.log('data source initialized');
 
-app.get("/", (req, res) => {
-  res.send("Hello, TypeScript Node Express!");
-});
+  const app = express();
+  const port = 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan('tiny'));
+
+  app.get('/', (req, res) => {
+    res.send('Hello, TypeScript Node Express!');
+  });
+
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+bootstrap();
